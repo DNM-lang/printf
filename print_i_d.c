9a4b054fac6587101e6684f7359ec7640965b66e
blessi30ng <blessingchidewu3@gmail.com>
2023-11-14 07:35:28 -0800

    lolll

--- print_i_d.c
+++ print_i_d.c
@@ -1,43 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
-/**
- * _printf_i_d - prints the charscters
- * @format: pointer to the elements.
- * Return: printed charscter
- */
-int _printf_i_d(const char *format, ...)
-{
-	va_list args;
-	int printed_chars = 0;
 
-	va_start(args, format);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'd':
-				case 'i':
-					printed_chars += _printf_int(va_arg(args, int));
-					break;
-				default:
-					_putchar('%');
-					_putchar(*format);
-					printed_chars += 2;
-					break;
-			}
-		}
-		else
-		{
-			printed_chars += _putchar(*format);
-		}
-		format++;
-	}
-	va_end(args);
-	return (printed_chars);
-}
 /**
  * _printf_int - prints the d and i
  * @num: the number to be printed
