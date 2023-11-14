e8c6651908d1c6f9bbdbcbe853bbea9a2f383535
blessi30ng <blessingchidewu3@gmail.com>
2023-11-14 09:04:42 -0800

    leasse

--- main.h
+++ main.h
@@ -6,8 +6,7 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _printf_int(int num);
-int print_perc(void);
-int _printf_i_d(const char *format, ...);
+int print_perc(void)
 int print_c(va_list val);
 int _printf_string(char *s);
 
