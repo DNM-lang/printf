64d933b4970c05eab1775d2e428ab3003c5cd0b3
DNM-lang <njorogedennis59@gmail.com>
2023-11-14 17:32:15 -0800

    update

--- _printf.c
+++ _printf.c
@@ -89,3 +89,23 @@ int print_perc(void)
 	_putchar(37);
 	return (1);
 }
+/**
+ * _printf_int - prints the d and i
+ * @num: the number to be printed
+ * Return: count
+ */
+int _printf_int(int num)
+{
+	int count = 0;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+		count++;
+	}
+	if (num / 10)
+		count += _printf_int(num / 10);
+	_putchar(num % 10 + '0');
+	return (count + 1);
+}
